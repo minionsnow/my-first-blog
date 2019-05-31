b19987310a606981544384ace22d091a9e3a089f
yash <pandityhre321@gmail.com>
2019-05-31 15:13:06 +0530

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ypandit\appdata\local\programs\python\python37\scripts\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
