8b914fc3d6ba9c4c88d341c31ae8bd7b0cf12ee3
yash <pandityhre321@gmail.com>
2019-06-18 10:12:57 +0530

    new changes

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
+    path('output/', views.output ,name='script'),
+    path('summary/', views.summary ,name='summary'),
+    #path('logs/', views.logs ,name='logs'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,69 @@
 from django.shortcuts import render
+from .forms import ContactForm
+import os
 
-# Create your views here.
+refe=[]
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def home(request):
+    refe[:]=[]
+    with open('summary.txt','w') as fs:
+        fs.truncate()
+    fs.close()
+    with open('results.txt','w') as fs:
+        fs.truncate()
+    fs.close()
+    if request.method=='POST':
+        print(refe)
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            host=form.cleaned_data['host']
+            console=form.cleaned_data['console']
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            print(host,console,username,password)
+            refe.append(host)
+            refe.append(console)
+            refe.append(username)
+            refe.append(password)
+    else :
+        form=ContactForm()
+    return render(request, 'blog/newhome.html',{'form':form})
+def output(request):
+    try:
+        cmd = 'python ./blog/pyth.py '+refe[0]+' '+refe[1]+' '+refe[2]+' '+refe[3]
+        print(cmd)
+        os.system(cmd)
+        with open('results.txt') as fs:
+            data=fs.read()
+        data = data.replace('\n', "<br />");
+        fs.close()
+        return render(request, 'blog/results.html', {'data':data})
+    except:
+        data = 'Sorry Error happened'
+        return render(request, 'blog/results.html', {'data':data})
+def summary(request):
+    try:
+        with open('summary.txt') as fs:
+            data=fs.read()
+        data = data.replace('\n', "<br />");
+        fs.close()
+        return render(request, 'blog/run.html', {'data':data})
+    except:
+        data = 'Sorry Error happened'
+        return render(request, 'blog/run.html', {'data':data})
+'''def logs(request):
+    try:
+        os.system('ssh bgl-ads-1534')
+        os.system('Yhre321#78\n')
+        cmd='cd /auto/tftp-blr-users1/ypandit/traces/'
+        print(cmd)
+        os.system(cmd)
+        cmd='ls'
+        os.system(cmd)
+        cmd='exit'
+        os.system(cmd)
+        data='Hello'
+        return render(request, 'blog/logs.html', {'data':data})
+    except:
+        data = 'Sorry Error happened'
+        return render(request, 'blog/logs.html', {'data':data})'''
